1ae6aa4ca81638d7b66916ed839e09384e8fdc97
rg <ramilgojayev2003@gmail.com>
2022-03-20 21:49:51 +0100

    Initial Commit. (BasicAuth provided by SpringSecurity)

---
+++ src/main/java/portfolio/rg/springsecuritybook/SpringSecurityBookApplication.java
@@ -0,0 +1,13 @@
+package portfolio.rg.springsecuritybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/portfolio/rg/springsecuritybook/SpringSecurityBookApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.rg.springsecuritybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
