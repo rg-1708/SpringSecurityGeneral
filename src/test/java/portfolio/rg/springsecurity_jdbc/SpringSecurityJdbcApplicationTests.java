e8b6c62cfa67cce07bc7905040d23a4b002b4054
rg <ramilgojayev2003@gmail.com>
2022-03-21 17:34:38 +0100

    Initial commit -> JdbcUserDetailsManager showcase (3rd chapter).

---
+++ src/test/java/portfolio/rg/springsecurity_jdbc/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.rg.springsecurity_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
