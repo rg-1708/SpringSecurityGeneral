375b8b99f2093a87b67d9835d48468042b0c4237
rg <ramilgojayev2003@gmail.com>
2022-03-21 21:43:58 +0100

    JdbcUserDetailsManager implementation showcase (3rd chapter).

---
+++ src/main/java/portfolio/rg/springsecurity_jdbc/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package portfolio.rg.springsecurity_jdbc.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
