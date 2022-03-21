c810b143cb4f76047eb9947de773a1795468279a
rg <ramilgojayev2003@gmail.com>
2022-03-21 02:10:32 +0100

    Added InMemoryUserDetailsService, and a User. Temporarily deletes persistence from the project.

--- src/main/java/portfolio/rg/springsecuritybook/config/UserManagementConfig.java
+++ src/main/java/portfolio/rg/springsecuritybook/config/UserManagementConfig.java
@@ -3,27 +3,24 @@ package portfolio.rg.springsecuritybook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import portfolio.rg.springsecuritybook.model.User;
+import portfolio.rg.springsecuritybook.services.InMemoryUserDetailsService;
+
+import java.util.List;
 
 @Configuration
 public class UserManagementConfig {
+
     @Bean
     public UserDetailsService userDetailsService() {
-
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
+        UserDetails u = new User("john", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/portfolio/rg/springsecuritybook/model/User.java
+++ src/main/java/portfolio/rg/springsecuritybook/model/User.java
@@ -1,4 +1,4 @@
-package portfolio.rg.springsecuritybook.User;
+package portfolio.rg.springsecuritybook.model;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -6,41 +6,39 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class SecurityUser implements UserDetails {
-
-    private final User user;
-
-    public SecurityUser(User user) {
-        this.user = user;
-    }
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-    @Override
-    public String getPassword() {
-        return user.getPassword();
+public class User implements UserDetails {
+    private final String username;
+    private final String password;
+    private final String authority;
+    public User(String username, String password, String authority) {
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> user.getAuthority());
+        return List.of(() -> authority);
     }
-
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return username;
+    }
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @Override
     public boolean isEnabled() {
         return true;
