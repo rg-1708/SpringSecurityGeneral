022b78e40ee889c4a1ddeab54f7e629e4810b19a
rg <ramilgojayev2003@gmail.com>
2022-03-22 16:35:48 +0100

    Added DelegatingPasswordEncoder, which provides us ability to choose from different options,  we use Bcrypt encoder.

--- src/main/java/portfolio/rg/springsecuritybook/config/UserManagementConfig.java
+++ src/main/java/portfolio/rg/springsecuritybook/config/UserManagementConfig.java
@@ -5,13 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import portfolio.rg.springsecuritybook.model.User;
 import portfolio.rg.springsecuritybook.services.InMemoryUserDetailsService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class UserManagementConfig {
@@ -24,6 +29,10 @@ public class UserManagementConfig {
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
