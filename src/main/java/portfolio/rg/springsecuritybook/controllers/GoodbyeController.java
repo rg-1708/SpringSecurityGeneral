8e965cfdf0e71b5bee155912b4d3a9d4b431e740
rg <ramilgojayev2003@gmail.com>
2022-03-23 21:47:17 +0100

    Making use of DelegatingSecurityContextCallable.

--- src/main/java/portfolio/rg/springsecuritybook/controllers/GoodbyeController.java
+++ src/main/java/portfolio/rg/springsecuritybook/controllers/GoodbyeController.java
@@ -2,10 +2,15 @@ package portfolio.rg.springsecuritybook.controllers;
 
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class GoodbyeController {
@@ -16,6 +21,25 @@ public class GoodbyeController {
         SecurityContext context = SecurityContextHolder.getContext();
         String username = context.getAuthentication().getName();
         // do something with the username
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        //DelegatingSecurityContextCallable ->
+        //Provides security context to the current task,
+        //which is defined by developers themselves
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
     }
 
 }
