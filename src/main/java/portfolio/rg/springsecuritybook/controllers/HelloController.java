b44a2e9e224157b38f0c01a328e14d44870aab1f
rg <ramilgojayev2003@gmail.com>
2022-03-24 00:00:33 +0100

    Added DelegatingSecurityContextExecutorService, which decorates an ExecutorService.

--- src/main/java/portfolio/rg/springsecuritybook/controllers/HelloController.java
+++ src/main/java/portfolio/rg/springsecuritybook/controllers/HelloController.java
@@ -1,10 +1,15 @@
 package portfolio.rg.springsecuritybook.controllers;
 
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -12,4 +17,27 @@ public class HelloController {
     public String hello(Authentication a) {
         return "Hello, " + a.getName() + "!";
     }
+
+    @GetMapping("/hola")
+    public String hola() throws Exception {
+
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        //Now instead of wrapping the task itself,
+        //We use DelegatingSecurityContextExecutorService
+        //To manage the task, and provide security context to it
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        e = new DelegatingSecurityContextExecutorService(e);
+        try {
+            return "Hola, " + e.submit(task).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
 }
+
+
