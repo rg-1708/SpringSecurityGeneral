edb4495b221ec2c65a869ea57375ed7f126c1714
rg <ramilgojayev2003@gmail.com>
2022-03-23 01:59:29 +0100

    Added CustomAuthenticationProvider.

--- src/main/java/portfolio/rg/springsecuritybook/config/WebAuthorizationConfig.java
+++ src/main/java/portfolio/rg/springsecuritybook/config/WebAuthorizationConfig.java
@@ -1,12 +1,29 @@
 package portfolio.rg.springsecuritybook.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public WebAuthorizationConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        //Adding the custom AuthProvider
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
