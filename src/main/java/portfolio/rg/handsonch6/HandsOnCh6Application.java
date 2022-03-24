472aaec56e4be83499c99d637de617f3b243de43
rg <ramilgojayev2003@gmail.com>
2022-03-24 16:43:00 +0100

    Initial commit.

---
+++ src/main/java/portfolio/rg/handsonch6/HandsOnCh6Application.java
@@ -0,0 +1,13 @@
+package portfolio.rg.handsonch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnCh6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnCh6Application.class, args);
+    }
+
+}
---
+++ src/test/java/portfolio/rg/handsonch6/HandsOnCh6ApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.rg.handsonch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnCh6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
