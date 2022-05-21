ac6a721e8c375dea49ed1f0947f12d24ee79975b
rg <ramilgojayev2003@gmail.com>
2022-05-20 23:17:10 -0500

    Initial Commit.

---
+++ src/main/java/portfolio/rg/authorization_ch7/AuthorizationCh7Application.java
@@ -0,0 +1,13 @@
+package portfolio.rg.authorization_ch7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationCh7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationCh7Application.class, args);
+    }
+
+}
---
+++ src/test/java/portfolio/rg/authorization_ch7/AuthorizationCh7ApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.rg.authorization_ch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationCh7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
